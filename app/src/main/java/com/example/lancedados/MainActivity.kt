9068686058e9a8d030a301e9bbabb1d333631025
Louise Josephine <loophinesj@gmail.com>
2020-10-24 10:37:18 -0300

    Adiciona lógica dos dados.

--- app/src/main/java/com/example/lancedados/MainActivity.kt
+++ app/src/main/java/com/example/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,24 @@ class MainActivity : AppCompatActivity() {
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
 
-        botaoLancarDados.setOnClickListener {
+        val ImgDado1 = findViewById<ImageView>(R.id.dado1)
+        val ImgDado2 = findViewById<ImageView>(R.id.dado2)
 
+        botaoLancarDados.setOnClickListener {
+            val numero = lancarDados()
+            ImgDado1.setImageResource(escolherImg(numero))
+            ImgDado2.setImageResource(escolherImg(numero))
+        }
+    }
+
+    fun escolherImg(numero: Int): Int {
+        return when(numero){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else  -> R.drawable.dice_6
         }
     }
 
